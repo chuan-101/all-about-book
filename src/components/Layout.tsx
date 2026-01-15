35d81f0c216cb9b056ff97ead06a3f2a6d9ab4ce
chuan-101 <chuanchuan528@gmail.com>
2026-01-15 15:57:58 +0800

    Revamp discussion chat layout

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -3,7 +3,6 @@ import { NavLink, Outlet, useLocation, useNavigate } from 'react-router-dom'
 import BottomNav from './BottomNav'
 import { useAppData } from '../lib/app-context'
 import SyzygyConsole from './SyzygyConsole'
-import ThemeToggle from './ThemeToggle'
 import UpdatePrompt from './UpdatePrompt'
 
 type LayoutProps = {
@@ -59,33 +58,8 @@ function Layout({ onOpenSettings }: LayoutProps) {
             <div className="header-masthead-center">
               <h1 className="title">All About Book</h1>
             </div>
-            <div className="header-masthead-right">
-              <button
-                type="button"
-                className="icon-btn header-settings"
-                aria-label="Settings"
-                onClick={onOpenSettings}
-              >
-                <svg viewBox="0 0 24 24" aria-hidden="true">
-                  <path
-                    d="M12 8.2a3.8 3.8 0 1 0 0 7.6 3.8 3.8 0 0 0 0-7.6z"
-                    fill="none"
-                    stroke="currentColor"
-                    strokeWidth="1.6"
-                  />
-                  <path
-                    d="M4.8 12a7.2 7.2 0 0 1 .12-1.2l-2.12-1.65 1.9-3.3 2.5 1a7.6 7.6 0 0 1 2.1-1.2l.4-2.7h3.8l.4 2.7a7.6 7.6 0 0 1 2.1 1.2l2.5-1 1.9 3.3-2.12 1.65c.08.4.12.8.12 1.2s-.04.8-.12 1.2l2.12 1.65-1.9 3.3-2.5-1a7.6 7.6 0 0 1-2.1 1.2l-.4 2.7h-3.8l-.4-2.7a7.6 7.6 0 0 1-2.1-1.2l-2.5 1-1.9-3.3 2.12-1.65c-.08-.4-.12-.8-.12-1.2z"
-                    fill="none"
-                    stroke="currentColor"
-                    strokeWidth="1.2"
-                    strokeLinejoin="round"
-                  />
-                </svg>
-              </button>
-            </div>
           </div>
           <div className="header-actions">
-            <ThemeToggle />
             <nav className="nav">
               <NavLink to="/" end>
                 首页
